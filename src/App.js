416efb56d2213dd24a445da905509efbd8e9cd7f
Keigo Takahashi <keigo@TK.local>
2023-10-14 16:28:04 +0900

    Navbar追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
-import Home from './components/Home';
-import Login from './components/Login';
-import CreatePost from './components/CreatePost';
-import Logout from './components/Logout';
+import Home from './components/Home'
+import Login from './components/Login'
+import CreatePost from './components/CreatePost'
+import Logout from './components/Logout'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/createpost" element={<CreatePost />}></Route>
