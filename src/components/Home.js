0f55ffefe91043833ee37ae174c6b340e7ddafaa
Keigo Takahashi <keigo@TK.local>
2023-10-15 10:01:12 +0900

    記事削除処理追加

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import "./Home.css"
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../firebase';
+import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
+import { auth, db } from '../firebase';
 
 const Home = () => {
   const [postList, setPostList] = useState([]);
@@ -9,16 +9,21 @@ const Home = () => {
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(collection(db, "posts"));
-      setPostList(data.docs.map((doc) => ({...doc.data()})));
+      setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
     };
     getPosts();
   }, [])
+
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "posts", id));
+    window.location.href = "/";
+  };
 
   return (
     <div className="homePage">
       {postList.map((post) => {
         return (
-          <div className="postContents" key={post.author.id}>
+          <div className="postContents" key={post.id}>
             <div className="postHeader">
               <h1>{post.title}</h1>
             </div>
@@ -27,7 +32,9 @@ const Home = () => {
             </div>
             <div className="nameAndDeleteButton">
               <h3>@{post.author.username}</h3>
-              <button>削除</button>
+              {post.author.id === auth.currentUser?.uid && (
+                <button onClick={() => handleDelete(post.id)}>削除</button>
+              )}
             </div>
           </div>
         ); 
